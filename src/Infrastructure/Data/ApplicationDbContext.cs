c17b78c6246989ce8029d8a1af85218ced6a61c7
salman <salman.shafiq@asdbd.com>
2025-03-12 16:27:12 +0600

    ApplicationDbContext changed

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,13 +1,11 @@
 ﻿using System.Reflection;
 using ASD.SeedProjectNet8.Application.Common.Interfaces;
 using ASD.SeedProjectNet8.Domain.Entities;
-using ASD.SeedProjectNet8.Infrastructure.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ASD.SeedProjectNet8.Infrastructure.Data;
 
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplicationDbContext
+public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
